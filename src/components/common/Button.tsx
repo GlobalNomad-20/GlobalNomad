314eb87a270e51851a5857b800b7aa584765a6da
BZzzzi <khj23465@naver.com>
2026-01-26 22:48:38 +0900

    Feat: 공통 버튼 컴포넌트 구현
    - 아이콘 지원
    - variant 설정 지원

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,40 +1,24 @@
-import { ButtonHTMLAttributes } from "react";
+import { ButtonHTMLAttributes, ReactNode } from "react";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  variant?: "primary" | "secondary" | "outline";
-  size?: "sm" | "md" | "lg";
+  variant?: "primary" | "disabled" | "outline";
+  icon?: ReactNode;
 }
 
-const Button = ({
-  children,
-  variant = "primary",
-  size = "md",
-  className = "",
-  ...props
-}: ButtonProps) => {
-  // 기본 스타일
+const Button = ({ children, variant = "primary", icon, className = "", ...props }: ButtonProps) => {
   const baseStyles =
-    "rounded-md font-medium transition-colors disabled:opacity-50 disabled:cursor-not-allowed";
+    "py-3 md:py-[14.5px] lg:py-[17.5px] rounded-[12px] md:rounded-[14px] lg:rounded-[16px] transition-colors flex items-center justify-center gap-1";
 
-  // variant별 스타일
   const variantStyles = {
-    primary: "bg-blue-600 text-white hover:bg-blue-700",
-    secondary: "bg-gray-600 text-white hover:bg-gray-700",
-    outline: "border-2 border-blue-600 text-blue-600 hover:bg-blue-50",
-  };
-
-  // size별 스타일
-  const sizeStyles = {
-    sm: "px-3 py-1.5 text-sm",
-    md: "px-4 py-2 text-base",
-    lg: "px-6 py-3 text-lg",
+    primary: "bg-primary-500 hover:bg-[#3d95e2] text-white typo-14-b md:typo-16-b cursor-pointer",
+    disabled: "bg-gray-200 text-white typo-14-b md:typo-16-b cursor-not-allowed",
+    outline:
+      "border-1 border-gray-200 text-gray-600 hover:bg-gray-25 typo-14-m md:typo-16-m cursor-pointer",
   };
 
   return (
-    <button
-      className={`${baseStyles} ${variantStyles[variant]} ${sizeStyles[size]} ${className}`}
-      {...props}
-    >
+    <button className={`${baseStyles} ${variantStyles[variant]} ${className}`} {...props}>
+      {icon && <span className="shrink-0">{icon}</span>}
       {children}
     </button>
   );

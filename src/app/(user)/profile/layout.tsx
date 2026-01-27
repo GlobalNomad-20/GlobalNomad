327d580aaf506d5789af411786e74616382e13f5
spde3289 <spde3289@naver.com>
2026-01-27 17:14:28 +0900

    Fix: 레이아웃 css 수정

--- src/app/(user)/profile/layout.tsx
+++ src/app/(user)/profile/layout.tsx
@@ -9,7 +9,7 @@ interface ProfileLayoutProps {
 const ProfileLayout = ({ children }: ProfileLayoutProps) => {
   return (
     <div
-      className="h-full md:mx-7.5 md:mt-7.5 md:flex md:gap-7.5 lg:m-0 lg:mx-auto lg:max-w-245
+      className="h-full md:mx-7.5 md:flex md:gap-7.5 md:pt-7.5 lg:m-0 lg:mx-auto lg:max-w-245
         lg:gap-12.5"
     >
       <ProfileNav />

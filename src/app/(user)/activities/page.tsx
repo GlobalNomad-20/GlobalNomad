5cbb39bc9e360233130b5d90098a61b3834fd740
seoyoung <nct830@naver.com>
2026-01-27 19:11:09 +0900

    feat : 배경 적용

---
+++ src/app/(user)/activities/page.tsx
@@ -0,0 +1,18 @@
+const Activities = () => {
+  return (
+    <div className="bg-white">
+      <section
+        className="relative h-[450px] overflow-hidden bg-gradient-to-b from-[#BBDDFF] via-[#F7FBFF]
+          to-white md:h-[800px]"
+      >
+        <img
+          src="/image/cloud.svg"
+          alt="구름"
+          className="absolute top-6 left-0 w-[200vw] max-w-none opacity-90 md:w-[1600px]"
+        />
+      </section>
+    </div>
+  );
+};
+
+export default Activities;

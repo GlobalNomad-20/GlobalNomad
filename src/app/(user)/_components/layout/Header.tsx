e46287f066b55632452f2c742a41fbcf359a77bc
spde3289 <spde3289@naver.com>
2026-01-28 15:12:29 +0900

    Feat: 로고 클릭시 /activities 경로 이동

--- src/app/(user)/_components/layout/Header.tsx
+++ src/app/(user)/_components/layout/Header.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
+import Link from "next/link";
 
 import UserAuthNav from "./UserAuthNav";
+
+import { ROUTES } from "@/constants/routes";
 
 const Header = () => {
   const isLogin = true;
@@ -9,12 +12,14 @@ const Header = () => {
     <header className="h-12 px-6 md:h-20 md:px-7.5">
       <div className="mx-auto flex h-full max-w-380 items-center justify-between">
         <h1 className="relative size-7 overflow-hidden md:w-43.5">
-          <Image
-            src="/image/smLogo.png"
-            fill
-            className="object-cover object-left"
-            alt="로고 이미지"
-          />
+          <Link href={ROUTES.ACTIVITIES.ROOT}>
+            <Image
+              src="/image/smLogo.png"
+              fill
+              className="object-cover object-left"
+              alt="로고 이미지"
+            />
+          </Link>
         </h1>
         <UserAuthNav isLogin={isLogin} userName="김지훈" profileImage="/image/profile.png" />
       </div>

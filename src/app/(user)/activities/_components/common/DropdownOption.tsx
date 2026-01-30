528f35f18e5606e0f382c7cba7f05c5e7e08880d
seoyoung <nct830@naver.com>
2026-01-31 00:58:38 +0900

    fix : 카테고리 뱃지 모바일 스크롤 기능 구현

--- src/app/(user)/activities/_components/common/DropdownOption.tsx
+++ src/app/(user)/activities/_components/common/DropdownOption.tsx
@@ -8,15 +8,15 @@ const DropdownOption = () => {
         <DropdownArrowSvg />
       </button>
 
-      <ul
+      {/* <ul
         className="absolute -right-2 z-20 w-30 overflow-hidden rounded-[18px] border border-gray-200
-          bg-white text-center shadow-md"
+          bg-white text-center shadow-md md:w-40"
       >
-        <li className="cursor-pointer px-4 py-2 hover:bg-gray-50">가격 낮은 순</li>
-        <li className="cursor-pointer border-t border-gray-100 px-4 py-2 hover:bg-gray-50">
+        <li className="cursor-pointer px-4 py-2 hover:bg-gray-50 md:py-4">가격 낮은 순</li>
+        <li className="cursor-pointer border-t border-gray-100 px-4 py-2 hover:bg-gray-50 md:py-4">
           가격 높은 순
         </li>
-      </ul>
+      </ul> */}
     </div>
   );
 };
--- src/app/(user)/activities/_components/filters/CategoryFilter.tsx
+++ src/app/(user)/activities/_components/filters/CategoryFilter.tsx
@@ -8,22 +8,24 @@ import WellbeingSvg from "@/assets/svg/WellbeingSvg";
 
 const CategoryFilter = () => {
   return (
-    <div className="mb-6 flex gap-2 md:mb-7.5 md:gap-5">
-      <CategoryBadge>
-        <MusicSvg className="h-3 w-3 hover:fill-white md:h-5 md:w-5" /> 문화 · 예술
-      </CategoryBadge>
-      <CategoryBadge>
-        <FoodSvg className="h-3 w-3 hover:fill-white md:h-5 md:w-5" /> 식음료
-      </CategoryBadge>
-      <CategoryBadge>
-        <TourSvg className="h-3 w-3 hover:fill-white md:h-5 md:w-5" /> 투어
-      </CategoryBadge>
-      <CategoryBadge>
-        <BusSvg className="h-3 w-3 hover:fill-white md:h-5 md:w-5" /> 관광
-      </CategoryBadge>
-      <CategoryBadge>
-        <WellbeingSvg className="h-3 w-3 hover:fill-white md:h-5 md:w-5" /> 웰빙
-      </CategoryBadge>
+    <div className="mb-6 overflow-x-auto md:mb-7.5">
+      <div className="scrollbar-hide flex w-max gap-2 px-1.5 md:gap-5">
+        <CategoryBadge>
+          <MusicSvg className="h-3 w-3 hover:fill-white md:h-5 md:w-5" /> 문화 · 예술
+        </CategoryBadge>
+        <CategoryBadge>
+          <FoodSvg className="h-3 w-3 hover:fill-white md:h-5 md:w-5" /> 식음료
+        </CategoryBadge>
+        <CategoryBadge>
+          <TourSvg className="h-3 w-3 hover:fill-white md:h-5 md:w-5" /> 투어
+        </CategoryBadge>
+        <CategoryBadge>
+          <BusSvg className="h-3 w-3 hover:fill-white md:h-5 md:w-5" /> 관광
+        </CategoryBadge>
+        <CategoryBadge>
+          <WellbeingSvg className="h-3 w-3 hover:fill-white md:h-5 md:w-5" /> 웰빙
+        </CategoryBadge>
+      </div>
     </div>
   );
 };

37f73af9cd71ebd61a9792a6e0c440213db49e53
seoyoung <nct830@naver.com>
2026-02-01 02:42:30 +0900

    fix  : 검색창 UI 스타일 수정

--- src/app/(user)/activities/_components/common/SearchBar.tsx
+++ src/app/(user)/activities/_components/common/SearchBar.tsx
@@ -5,8 +5,9 @@ const SearchBar = () => {
   return (
     <div
       className="flex h-[53px] w-full items-center rounded-2xl bg-white pt-[6px] pr-[8px] pb-[6px]
-        pl-[20px] shadow-[0_6px_10px_rgba(13,153,255,0.05)] md:h-[70px] md:pt-[10px] md:pr-3
-        md:pb-[10px] md:pl-8 lg:rounded-3xl"
+        pl-[20px] shadow-[0_6px_10px_rgba(13,153,255,0.05)] focus-within:shadow-lg
+        focus-within:shadow-sky-100 md:h-[70px] md:pt-[10px] md:pr-3 md:pb-[10px] md:pl-8
+        lg:rounded-3xl"
     >
       <SearchSvg className="h-4 w-4 text-gray-500" />
       <input
@@ -16,7 +17,7 @@ const SearchBar = () => {
       />
       <Button
         variant="primary"
-        className="md:typo-16-b h-[41px] w-[85px] active:bg-blue-500 md:h-[50px] md:w-30"
+        className="md:typo-16-b h-[41px] w-[85px] active:bg-sky-600 md:h-[50px] md:w-30"
       >
         검색하기
       </Button>

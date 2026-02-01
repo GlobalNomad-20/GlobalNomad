334f729b9ff8641948566c3021180c5450740b99
spde3289 <spde3289@naver.com>
2026-02-02 02:12:51 +0900

    Feat: 내 정보 페이지 (API 연결 제외)

--- src/app/(user)/profile/edit/page.tsx
+++ src/app/(user)/profile/edit/page.tsx
@@ -1,5 +1,11 @@
+import ProfileEditField from "./_components/ProfileEditField";
+
 const ProfilePage = () => {
-  return <div className="w-full">edit 페이지</div>;
+  return (
+    <div className="w-full px-6 pt-8.75 pb-8.75 md:p-0">
+      <ProfileEditField />
+    </div>
+  );
 };
 
 export default ProfilePage;

c7e6c352af10adb0bdc5dfda3c962c6b97acec8e
spde3289 <spde3289@naver.com>
2026-02-03 21:16:43 +0900

    Feat: 공고 삭제 요청 함수

--- src/api/myNotifications.ts
+++ src/api/myNotifications.ts
@@ -2,10 +2,16 @@ import { API_ENDPOINTS } from "@/constants/apiEndPoint";
 import { client } from "@/lib/client/client";
 import { NotificationResponse } from "@/types/notification";
 
-export const getNotifications = async (size: number = 10, cursorId?: number) => {
+export const fetchMyNotifications = async (size?: number, cursorId?: number | null) => {
   const response = await client.get<NotificationResponse>(API_ENDPOINTS.MY_NOTIFICATIONS.LIST, {
     params: { size, cursorId },
   });
+
+  return response.data;
+};
+
+export const deleteMyNotification = async (notificationId: number) => {
+  const response = await client.delete(API_ENDPOINTS.MY_NOTIFICATIONS.DELETE(notificationId));
 
   return response.data;
 };

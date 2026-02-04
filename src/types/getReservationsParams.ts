77be923659d469c155f8ce7086b6ea108a1409c2
BZzzzi <khj23465@naver.com>
2026-02-05 00:47:55 +0900

    Refactor: 예약 관련 타입 정의 위치 변경 및 통합

--- src/types/getReservationsParams.ts
+++
@@ -1,7 +0,0 @@
-export type ReservationStatus = "pending" | "confirmed" | "declined" | "canceled" | "completed";
-
-export interface GetMyReservationsParams {
-  cursorId?: number;
-  size?: number;
-  status?: ReservationStatus;
-}
--- src/types/reservations.ts
+++ src/types/reservations.ts
@@ -1,4 +1,10 @@
-import { ReservationStatus } from "./getReservationsParams";
+export type ReservationStatus = "pending" | "confirmed" | "declined" | "canceled" | "completed";
+
+export interface GetReservationsParams {
+  cursorId?: number;
+  size?: number;
+  status?: ReservationStatus;
+}
 
 export interface ReservationActivity {
   bannerImageUrl: string;
@@ -27,4 +33,9 @@ export interface ReservationsResponse {
   cursorId: number;
   reservations: Reservation[];
   totalCount: number;
+}
+
+export interface ReservationReviewRequest {
+  rating: number;
+  content: string;
 }

8173b44ceb5ab164e399e697ce8a7fa470bf052d
seoyoung <nct830@naver.com>
2026-02-06 02:21:35 +0900

    feat : 상세페에지 체험 후기 섹션 UI 구현

--- src/app/(user)/activities/[id]/page.tsx
+++ src/app/(user)/activities/[id]/page.tsx
@@ -1,16 +1,20 @@
 import ActivityDescriptionSection from "../_components/sections/detail/ActivityDescriptionSection";
 import ActivityHeaderSection from "../_components/sections/detail/ActivityHeaderSection";
 import ActivityImageSection from "../_components/sections/detail/ActivityImageSection";
+import ActivityMapSection from "../_components/sections/detail/ActivityMapSection";
+import ActivityReviewSection from "../_components/sections/detail/ActivityReviewSection";
 
 const ActivityDetail = () => {
   return (
-    <div>
-      <div>
-        <div>
+    <div className="flex justify-center bg-white">
+      <div className="mt-7.5 md:mt-8.5 lg:mt-22">
+        <div className="flex flex-col gap-5 md:gap-6 lg:flex-row lg:gap-10">
           <ActivityImageSection />
           <ActivityHeaderSection />
         </div>
         <ActivityDescriptionSection />
+        <ActivityMapSection />
+        <ActivityReviewSection />
       </div>
     </div>
   );
---
+++ src/app/(user)/activities/_components/sections/detail/ActivityMapSection.tsx
@@ -0,0 +1,14 @@
+const ActivityMapSection = () => {
+  return (
+    <div
+      className="flex h-64.75 w-81.75 flex-col gap-2 md:h-103.25 md:w-171 md:gap-3.5 lg:h-137.75
+        lg:w-167.5 lg:gap-2"
+    >
+      <div className="typo-16-b md:typo-18-b text-gray-950">오시는 길</div>
+      <div className="text-sm font-semibold text-gray-950">서울 중구 청계천로 100 10F</div>
+      <div className="h-45 w-full rounded-2xl bg-gray-500 md:h-75 lg:h-112.5"></div>
+    </div>
+  );
+};
+
+export default ActivityMapSection;

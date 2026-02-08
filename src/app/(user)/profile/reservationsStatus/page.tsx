11bdf878469ca9e0014088176919b3d5f968db90
spde3289 <spde3289@naver.com>
2026-02-08 19:04:41 +0900

    Feat: 예약 내역 현황에 따른 분기처리 컴포넌트

--- src/app/(user)/profile/reservationsStatus/page.tsx
+++ src/app/(user)/profile/reservationsStatus/page.tsx
@@ -1,15 +1,21 @@
-import EmptyReservationSection from "./_components/EmptyReservationSection";
+import ReservationContent from "./_components/ReservationContent";
+
+import { CalendarEvent } from "@/components/calenderView/CalendarEventItem";
 
 const ReservationsStatusPage = () => {
+  const myEvents: Record<string, CalendarEvent[]> = {
+    "2026-02-14": [{ id: 1, title: "예약 2", type: "예약" }],
+  };
+
   return (
-    <div className="w-full">
-      <div className="mb-11 py-2.5">
+    <div className="mb-3.5 w-full pt-7.5 md:mb-24 md:p-0">
+      <div className="mb-11 px-6 py-2.5 md:px-0">
         <h2 className="typo-18-b mb-2.5">예약 현황</h2>
         <p className="typo-14-m text-gray-500">
           내 체험에 예약된 내역들을 한 눈에 확인할 수 있습니다.
         </p>
       </div>
-      <EmptyReservationSection />
+      <ReservationContent reservations={myEvents} />
     </div>
   );
 };

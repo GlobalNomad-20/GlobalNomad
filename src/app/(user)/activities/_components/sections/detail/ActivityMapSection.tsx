8537a7d1b5f5c7672a87faf51ff7682407d3df92
seoyoung <nct830@naver.com>
2026-02-10 23:05:35 +0900

    feat : 상세페이지 오시는 길 api 연결 및 카카오맵 SDK 지도 기능 구현

--- src/app/(user)/activities/_components/sections/detail/ActivityMapSection.tsx
+++ src/app/(user)/activities/_components/sections/detail/ActivityMapSection.tsx
@@ -1,3 +1,5 @@
+import KakaoMap from "../../common/KakaoMap";
+
 import { ActivityDetailResponse } from "@/types/activityIdParams";
 
 interface ActivityMapSectionProps {
@@ -8,7 +10,12 @@ const ActivityMapSection = ({ data }: ActivityMapSectionProps) => {
     <div className="flex w-81.75 flex-col gap-2 md:w-171 md:gap-3.5 lg:w-167.5 lg:gap-2">
       <div className="typo-16-b md:typo-18-b text-gray-950">오시는 길</div>
       <div className="text-sm font-semibold text-gray-950">{data?.address}</div>
-      <div className="h-45 w-full rounded-2xl bg-gray-500 md:h-75 lg:h-112.5"></div>
+      <div
+        id="map"
+        className="h-45 w-full overflow-hidden rounded-2xl bg-gray-500 md:h-75 lg:h-112.5"
+      >
+        <KakaoMap address={data?.address} />
+      </div>
     </div>
   );
 };

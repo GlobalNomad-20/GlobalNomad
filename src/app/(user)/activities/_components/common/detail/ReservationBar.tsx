81a85ebe8fbd9b1f7a937eae22b2246b4f9ea20d
seoyoung <nct830@naver.com>
2026-02-12 00:53:54 +0900

    feat : 상세페이지 예약캘린더 UI 모바일태블릿 고정바,모달 구현

---
+++ src/app/(user)/activities/_components/common/detail/ReservationBar.tsx
@@ -0,0 +1,69 @@
+import { motion } from "framer-motion";
+
+import ReservationModal from "./ReservationModal";
+
+import Button from "@/components/common/Button";
+import Modal from "@/components/common/Modal";
+import { useModal } from "@/hooks/useModal";
+import { ActivityDetailResponse } from "@/types/activityIdParams";
+
+interface ReservationBarProps {
+  data?: ActivityDetailResponse;
+}
+
+const ReservationBar = ({ data }: ReservationBarProps) => {
+  const { isOpen, onOpen, onClose } = useModal();
+
+  const handleOpen = () => {
+    onOpen();
+  };
+
+  const handleCancel = () => {
+    onClose();
+  };
+
+  const handleComplete = () => {
+    onClose();
+  };
+
+  return (
+    <div
+      className="py sticky bottom-0 z-20 flex h-31 w-full flex-col justify-center gap-3 border-t
+        border-[#E6E6E6] bg-white px-6 py-4.5"
+    >
+      <div className="flex justify-between">
+        <div className="flex items-center gap-1.5">
+          <div className="typo-18-b text-gray-950">₩ {data?.price}</div>
+          <div className="typo-16-m text-[#79747E]">/ 1명</div>
+        </div>
+        <div
+          className="typo-16-b text-primary-500 underline active:text-sky-600"
+          onClick={handleOpen}
+        >
+          날짜 선택하기
+        </div>
+        {isOpen && (
+          <Modal
+            isOpen={isOpen}
+            onClose={handleCancel}
+            position="bottom"
+            containerClassName="h-[605px]"
+          >
+            <motion.div
+              initial={{ y: 100 }}
+              animate={{ y: 0 }}
+              transition={{ duration: 0.3, ease: "easeOut" }}
+            >
+              <ReservationModal onCancel={handleCancel} onComplete={handleComplete} />
+            </motion.div>
+          </Modal>
+        )}
+      </div>
+      <Button variant="disabled" className="w-full">
+        예약하기
+      </Button>
+    </div>
+  );
+};
+
+export default ReservationBar;
---
+++ src/app/(user)/activities/_components/common/detail/ReservationModal.tsx
@@ -0,0 +1,23 @@
+/* eslint-disable react/jsx-handler-names */
+import Button from "@/components/common/Button";
+
+interface ReservationModalProps {
+  onCancel: () => void;
+  onComplete: () => void;
+}
+
+const ReservationModal = ({ onCancel, onComplete }: ReservationModalProps) => {
+  return (
+    <div className="flex h-full flex-col p-6 md:px-7.5">
+      <p className="typo-18-b md:typo-20-b mb-2 text-gray-950 md:mb-6">날짜</p>
+      <div className="flex gap-3">
+        <Button onClick={onCancel} variant="outline">
+          아니오
+        </Button>
+        <Button onClick={onComplete}>실행하기</Button>
+      </div>
+    </div>
+  );
+};
+
+export default ReservationModal;

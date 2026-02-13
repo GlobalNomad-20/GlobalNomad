b1bf6fb4e96e280d9ba4af52660363e7ff5266ae
spde3289 <spde3289@naver.com>
2026-02-13 20:06:35 +0900

    Feat: 팝업 선택 로직 추가

--- src/app/(user)/profile/(with-nav)/reservationsStatus/_components/CalenderSection.tsx
+++ src/app/(user)/profile/(with-nav)/reservationsStatus/_components/CalenderSection.tsx
@@ -1,22 +1,31 @@
 "use client";
 
+import { useState } from "react";
+
 import ReservationsListPopup from "./popup/ReservationsListPopup";
 
 import { CalendarEvent } from "@/components/calenderView/CalendarEventItem";
 import CalendarView from "@/components/calenderView/CalenderView";
+import { Activity } from "@/types/activityCardList";
 
 interface CalendarSectionProps {
   events: Record<string, CalendarEvent[]>;
 }
 
 const CalendarSection = ({ events }: CalendarSectionProps) => {
+  const [selectedActivity, setSelectedActivity] = useState<Activity | null>(null);
+
+  const handleSelectedAcivity = (activity: Activity) => {
+    setSelectedActivity(activity);
+  };
+
   const handleDateClick = (date: string) => {
     console.log(`${date} 클릭됨`);
   };
 
   return (
     <>
-      <ReservationsListPopup />
+      <ReservationsListPopup selectedActivity={selectedActivity} onClick={handleSelectedAcivity} />
       <CalendarView events={events} onDateClick={handleDateClick} />
     </>
   );
--- src/app/(user)/profile/(with-nav)/reservationsStatus/_components/popup/ReservationsListPopup.tsx
+++ src/app/(user)/profile/(with-nav)/reservationsStatus/_components/popup/ReservationsListPopup.tsx
@@ -1,15 +1,66 @@
-import DropdownArrowSvg from "@/assets/svg/DropdownArrowSvg";
+"use client";
 
-const ReservationsListPopup = ({}) => {
+import ReservationsList from "./ReservationsList";
+
+import DropdownArrowSvg from "@/assets/svg/DropdownArrowSvg";
+import { useGetMyActivities } from "@/hooks/queries/useMyActivities";
+import { usePopup } from "@/hooks/usePopup";
+import { Activity } from "@/types/activityCardList";
+import { cn } from "@/utils/cn";
+
+interface ReservationsListPopupProps {
+  selectedActivity: Activity | null;
+  onClick: (activity: Activity) => void;
+}
+
+const ReservationsListPopup = ({
+  selectedActivity,
+  onClick: handleClick,
+}: ReservationsListPopupProps) => {
+  const { data, fetchNextPage, ...queryHelpers } = useGetMyActivities({
+    size: 10,
+  });
+  const { popupRef, triggerRef, open, handleToggle, handleClose } = usePopup();
+
+  const handleReachEnd = () => {
+    if (queryHelpers.hasNextPage && !queryHelpers.isFetchingNextPage) {
+      fetchNextPage();
+    }
+  };
+
   return (
-    <button
-      className="mb-4 flex w-full items-center justify-between rounded-2xl border border-gray-100
-        bg-white px-5 py-3.75 shadow-[0px_2px_6px_0px_#00000005] hover:cursor-pointer md:mb-6
-        lg:mb-7.5"
-    >
-      <h4 className="typo-14-m md:typo-16-m">함께 배우면 즐거운 스트릿 댄스</h4>
-      <DropdownArrowSvg />
-    </button>
+    <div className="relative">
+      <button
+        ref={triggerRef}
+        onClick={handleToggle}
+        className="mb-4 flex w-full items-center justify-between rounded-2xl border border-gray-100
+          bg-white px-5 py-3.75 shadow-[0px_2px_6px_0px_#00000005] hover:cursor-pointer md:mb-6
+          lg:mb-7.5"
+      >
+        <h4 className="typo-14-m md:typo-16-m">
+          {selectedActivity ? selectedActivity.title : "체험을 선택해주세요"}
+        </h4>
+        <DropdownArrowSvg />
+      </button>
+      {open && (
+        <div
+          ref={popupRef}
+          className={cn(
+            `absolute z-50 flex flex-col rounded-2xl border border-gray-100 bg-white px-2 py-4
+            shadow-[0px_2px_6px_0px_#00000005]`,
+            "top-15 right-0 left-auto w-full",
+          )}
+        >
+          <ReservationsList
+            data={data}
+            isFetchingNextPage={queryHelpers.isFetchingNextPage}
+            onReachEnd={handleReachEnd}
+            onClose={handleClose}
+            onClick={handleClick}
+          />
+        </div>
+      )}
+    </div>
   );
 };
 

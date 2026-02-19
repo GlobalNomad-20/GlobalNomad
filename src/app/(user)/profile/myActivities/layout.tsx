57869b59c34dca6d5406381403cedd72198534d7
BZzzzi <khj23465@naver.com>
2026-02-19 19:18:42 +0900

    Feat: 주소 검색 기능을 위한 AddressInput 및 useAddressSearch 추가
    - Daum 우편번호 서비스 SDK 스크립트 로드 로직 포함
    - 전역 window 객체 내 daum 속성 타입 선언 반영

--- src/app/(user)/profile/myActivities/layout.tsx
+++ src/app/(user)/profile/myActivities/layout.tsx
@@ -1,3 +1,4 @@
+import Script from "next/script";
 import { ReactNode } from "react";
 
 interface Props {
@@ -11,6 +12,10 @@ const MyActivityUpsertLayout = ({ children }: Props) => {
         className="mx-auto mb-18 max-w-175 px-6 pt-7.5 md:mb-12 md:px-7.5 md:pt-10 lg:mb-30 lg:px-0"
       >
         {children}
+        <Script
+          src="https://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js"
+          strategy="afterInteractive"
+        />
       </main>
     </>
   );

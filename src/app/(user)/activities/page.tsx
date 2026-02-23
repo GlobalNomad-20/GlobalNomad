388532b749dcf1072a6503d39b09fa975f9514ac
seoyoung <nct830@naver.com>
2026-02-24 02:21:17 +0900

    feat : 메인페이지 배경 구름 애니메이션 추가

--- src/app/(user)/activities/page.tsx
+++ src/app/(user)/activities/page.tsx
@@ -1,22 +1,24 @@
-import Image from "next/image";
-
 import ActivityBrowseSection from "./_components/sections/main/ActivityBrowseSection";
+import BannerSection from "./_components/sections/main/BannerSection";
 import PopularActivitySection from "./_components/sections/main/PopularActivitySection";
 import SearchSection from "./_components/sections/main/SearchSection";
 
-import BannerSection from "@/app/(user)/activities/_components/sections/main/BannerSection";
 const Activities = () => {
   return (
     <div className="bg-white">
       <div className="relative h-200 md:h-350">
         <div className="absolute inset-0 z-0 bg-linear-to-b from-[#BBDDFF] via-[#F7FBFF] to-white" />
         <div className="absolute top-0 left-0 z-10 h-35 w-full overflow-hidden md:h-70 lg:h-105">
-          <Image
-            src="/image/cloud.svg"
-            alt="구름"
-            fill
-            className="absolute bottom-6 left-0 w-[200vw] max-w-none opacity-90 md:w-400"
-          />
+          <div
+            className="h-full w-full transform-gpu animate-[cloud-float_6s_ease-in-out_infinite]
+              will-change-transform"
+          >
+            <div
+              className="h-full w-full animate-[cloud-scroll_100s_linear_infinite]
+                [background-image:url('/image/cloud.svg')] bg-size-[var(--cloud-tile-w-scaled)_100%]
+                bg-position-[0_100%] bg-repeat-x opacity-90 will-change-[background-position]"
+            />
+          </div>
         </div>
       </div>
       <div
